79797085a1667aec350c95190d5a1627bd2ab743
Ron Santos <ron.santos@gmail.com>
2014-09-08 15:09:05 -0700

    Correction from previous commit. Should be moving it 2 weeks later not earlier

--- lib/utils.rb
+++ lib/utils.rb
@@ -8,8 +8,8 @@ module Utils
   def registration_date(start_at)
     #start_date = DateTime.parse(start_at)
     #start_date - 70
-    #Registration date 12weeks or 84days before start of next term
-    Date.parse(start_at) - 84
+    #Registration date 8 weeks or 56 days before start of next term
+    Date.parse(start_at) - 56
   end
 
   def save_term_to_file(filename, term)
