30d6692495263de79eaa09a412725b0963894a96
Ron Santos <ron.santos@gmail.com>
2014-09-08 14:47:38 -0700

    After talking to Karen/Shaili from Student Services, need to push the registration date from 10 weeks to 12 weeks to start of next term

--- lib/utils.rb
+++ lib/utils.rb
@@ -8,8 +8,8 @@ module Utils
   def registration_date(start_at)
     #start_date = DateTime.parse(start_at)
     #start_date - 70
-    #Registration date 10weeks or 70days before start of next term
-    Date.parse(start_at) - 70
+    #Registration date 12weeks or 84days before start of next term
+    Date.parse(start_at) - 84
   end
 
   def save_term_to_file(filename, term)
