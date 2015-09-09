627cc4c6abe086cc27233c0a3b5f9c08c38513d3
Ron Santos <ron.santos@gmail.com>
2015-09-09 10:51:00 -0700

    Canvas API changed term API
    JSON data now wrapped in enrollment_term object

--- lib/term.rb
+++ lib/term.rb
@@ -9,23 +9,23 @@ class Term
       @term = Canvas::Server.parse_json("#{Canvas::Server.server}/sfu/api/v1/terms/#{term_code}").last || {}
     else   
       @term = Canvas::Server.parse_json("#{Canvas::Server.server}/sfu/api/v1/terms/#{term_code}").first || {}
-    end  
+    end
   end
 
   def id
-    @term["id"]
+    @term["enrollment_term"]["id"]
   end
 
   def start_at
-    @term["start_at"]
+    @term["enrollment_term"]["start_at"]
   end
 
   def end_at
-    @term["end_at"]
+    @term["enrollment_term"]["end_at"]
   end
 
   def name
-    @term["name"]
+    @term["enrollment_term"]["name"]
   end
 
 end
